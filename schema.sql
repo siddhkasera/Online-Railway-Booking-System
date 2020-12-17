a35d81aa13ddc221bcb520fe74beb5546eb42c86
HeeralN <47676001+HeeralN@users.noreply.github.com>
2020-12-17 15:28:00 -0600

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -18,6 +18,9 @@ DROP TABLE IF EXISTS `admin`;
 CREATE TABLE `admin`(
   `adminuser` varchar(30) NOT NULL,
   `adminpass` varchar(30) NOT NULL,
+  `ssn` varchar(9) NOT NULL,
+  `first name` varchar(25) DEFAULT NULL,
+  `last name` varchar(25) DEFAULT NULL,
   primary key(`adminuser`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -42,9 +45,9 @@ DROP TABLE IF EXISTS `customerrep`;
 CREATE TABLE `customerrep`(
 	`repuser` varchar(30) NOT NULL,
 	`reppass` varchar(30) NOT NULL,
-	`ssn` varchar(9) NOT NULL,
-   	`firstname` varchar(25) DEFAULT NULL,
-	`lastname` varchar(25) DEFAULT NULL, 
+    	`ssn` varchar(9) NOT NULL,
+   	`first name` varchar(25) DEFAULT NULL,
+	`last name` varchar(25) DEFAULT NULL, 
 	primary key(`repuser`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -84,7 +87,7 @@ CREATE TABLE `reservation` (
   foreign key(`customeruser`) References `customer`(`customeruser`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
+ 
 DROP TABLE IF EXISTS `reserves`;
 /*!40101 SET @saved_cs_client = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -102,11 +105,11 @@ DROP TABLE IF EXISTS `transitLine`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `transitLine`(
-`transit-line-name` varchar(30) NOT NULL,
+`tlName` varchar(30) NOT NULL,
 `origin` int NOT NULL,
 `destination` int NOT NULL,
 `fixed fare` double NOT NULL,
-primary key(`transit-line-name`),
+primary key(`tlName`),
 foreign key (`origin`) References `station`(`stationId`),
 foreign key (`destination`) References `station`(`stationId`)
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -117,10 +120,10 @@ DROP TABLE IF EXISTS `Transit_line_stations`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Transit_line_stations`(
 `stationId` int NOT NULL,
-`transit-line-name` varchar(30) NOT NULL,
-primary key(`stationId`, `transit-line-name`),
+`tlName` varchar(30) NOT NULL,
+primary key(`stationId`, `tlName`),
 foreign key (`stationId`) References `station` (`stationId`),
-foreign key (`transit-line-name`) References `transitLine` (`transit-line-name`)
+foreign key (`tlName`) References `transitLine` (`tlName`)
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -129,10 +132,10 @@ DROP TABLE IF EXISTS `Train_line`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Train_line`(
 `trainId` int NOT NULL,
-`transit-line-name` varchar(30) NOT NULL,
-primary key(`trainId`, `transit-line-name`),
+`tlName` varchar(30) NOT NULL,
+primary key(`trainId`, `tlName`),
 foreign key (`trainId`) References `train` (`trainId`),
-foreign key (`transit-line-name`) References `transitLine` (`transit-line-name`)
+foreign key (`tlName`) References `transitLine` (`tlName`)
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -152,16 +155,14 @@ DROP TABLE IF EXISTS `Transit_line_route`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE  `Transit_line_route`(
-`transit-line-name` varchar(30) NOT NULL,
+`tlName` varchar(30) NOT NULL,
 `stopNum` int NOT NULL,
 `origin` int NOT NULL,
 `dest` int NOT NULL,
-`dept_time` datetime NOT NULL,
-`arrv_time` datetime NOT NULL,
-primary key(`transit-line-name`,`stopNum`),
+primary key(`tlName`,`stopNum`),
 foreign key (`origin`) References `station`(`stationID`),
 foreign key (`dest`) References `station`(`stationID`),
-foreign key (`transit-line-name`) References `transitLine`(`transit-line-name`)
+foreign key (`tlName`) References `transitLine`(`tlName`)
 )ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -176,21 +177,7 @@ foreign key (`reservation no.`) References `reservation` (`reservation no.`),
 foreign key (`sched_id`) References `Schedule` (`sched_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `messages`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `messages`(
-	`messageId` int NOT NULL AUTO_INCREMENT,
-	`customeruser` varchar(30) NOT NULL,
-    `repuser` varchar(30) DEFAULT NULL,
-    `subject` varchar(150) NOT NULL,
-    `content` varchar(500) DEFAULT NULL,
-    `answer` varchar(500) DEFAULT NULL,
-    primary key(`messageId`),
-    foreign key (`customeruser`) References `customer`(`customeruser`),
-    foreign key(`repuser`) References `customerrep`(`repuser`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-    
+
 LOCK TABLES `customer` WRITE;
 /*!40000 ALTER TABLE `customer` DISABLE KEYS */;
 insert into `customer`(`customeruser`, `email`, `first name`, `last name`, `customerpass`) VALUES
@@ -205,60 +192,67 @@ UNLOCK TABLES;
 
 LOCK TABLES `admin` WRITE;
 /*!40000 ALTER TABLE `admin` DISABLE KEYS */;
-Insert into `admin`(`adminuser`, `adminpass`) VALUES
-('hi333', 'hi1122');
+Insert into `admin`(`adminuser`, `adminpass`,`ssn`, `first name`, `last name`) VALUES
+('mia333', 'ma1122', '111-11111', 'Mia','Jones'),
+('hp909', 'had990','222-22222', 'Harper','Brown'),
+('vy888', 'vicy888', '333-33333','Victoria', 'Young');
 /*!40000 ALTER TABLE `admin` ENABLE KEYS */;
 UNLOCK TABLES;
 
 LOCK TABLES `customerrep` WRITE;
 /*!40000 ALTER TABLE `customerrep` DISABLE KEYS */;
-INSERT INTO `customerrep` (`repuser`, `reppass`,`ssn`,`firstname`, `lastname`) VALUES	
+INSERT INTO `customerrep` (`repuser`, `reppass`,`ssn`,`first name`, `last name`) VALUES	
 ('ma343', 'm345', '112-11111', 'Mia1', 'Jones1'),
 ('hp009', 'h666', '221-22111', 'Harper1', 'Brown1'),
 ('vy757', 'vyng433', '212-33445', 'Victoria2', 'Young5');
 /*!40000 ALTER TABLE `customerrep` ENABLE KEYS */;
 UNLOCK TABLES;
 
-LOCK TABLES `messages` WRITE;
-/*!40000 ALTER TABLE `messages` DISABLE KEYS */;
-INSERT INTO `messages`(`messageId`, `customeruser`, `repuser`, `subject`, `content`, `answer`) VALUES
-	('1', 'siddhik', 'ma343','Make a reseravtion', 'Not able to make a registration', 'Specific Issue?'),
-    ('2', 'heeraln', 'ma343','Unable to search', 'Why is search not working?', NULL),
-    ('3', 'siddhik', 'hp009','Login not working', 'I am unable to log into my account.', NULL);
-/*!40000 ALTER TABLE `messages` ENABLE KEYS */;
-UNLOCK TABLES;
-
 LOCK TABLES `train` WRITE;
 /*!40000 ALTER TABLE `train` DISABLE KEYS */;
-INSERT INTO `train`(trainId) VALUES (161),(251),(113),(152),(184),(244),(291),(313),(253),(287);
+INSERT INTO `train`(trainId) VALUES (100),(101),(102),(103),(104),(105),(106),(107),(108),(109);
 /*!40000 ALTER TABLE `train` ENABLE KEYS */;
 UNLOCK TABLES;
 
 LOCK TABLES `station` WRITE;
 /*!40000 ALTER TABLE `station` DISABLE KEYS */;
 INSERT INTO `station`(`stationId`,`state`, `name`, `city`) VALUES
-(11, 'NJ' ,'Atlantic City', 'Atlantic City')
-,(12,'NJ','Egg Harbor City','Egg Harbor City')
-,(13,'NJ','Hammonton', 'Atlantic County')
-,(14,'NJ','Cherry Hill','Camden')
-,(15,'NJ','Philadelphia','Philadelphia')
-,(16,'NJ','Trenton','Trenton')
-,(17,'NJ','Hamilton','Hamilton')
-,(18,'NJ','Princeton Junction','Princeton')
-,(19,'NJ','Jersey Avenue','New Brunswick')
-,(20,'NJ','New Brunswick','New Brunswick')
-,(21,'NJ','Edison','Edison')
-,(22,'NJ','Metuchen','Metuchen')
-,(23,'NJ','Elizabeth','Elizabeth')
-,(24,'NJ','Newark Penn Station','Newark')
-,(25,'NY','New York','New York')
-,(26,'NJ','Teterboro','Hasbrouck Heights')
-,(27,'NJ','Secaucus Junction','Secaucus')
-,(28,'NJ','Hoboken','Hoboken')
-,(29,'NJ','Red Bank','Red Bank')
-,(30,'NJ','Woodbridge','Woodbridge')
-,(31,'NJ','Linden','Linden');
+(11, 'NJ' ,'Metropark', 'Woodbridge')
+,(12,'NJ','Metuchen','Middlesex')
+,(13,'NJ','Edison','Edison')
+,(14,'NJ','New Brunswick', 'New Brunswick')
+,(15,'NJ','Dunellen','Middlesex')
+,(16,'NJ','Bound Brook','Somerset')
+,(17,'NJ','Bridgewater','Somerset')
+,(18,'NJ','Somerville','Somerset')
+,(19,'NJ','Newark Intl Airport','Newark')
+,(20,'NJ','Princeton Junction','Princeton')
+,(21,'NJ','Trenton','Trenton')
+,(22,'NJ','Philadelphia','Philadelphia')
+,(23,'NJ','Secaucus Junction','Secaucus')
+,(24,'NJ','Harrison','Hudson')
+,(25,'NJ','Elizabeth','Elizabeth');
 /*!40000 ALTER TABLE `station` ENABLE KEYS */;
+UNLOCK TABLES;
+
+#NOT FINISHED NOT FINISHED
+LOCK TABLES `Transit_line_route` WRITE;
+/*!40000 ALTER TABLE `Transit_line_route` DISABLE KEYS */;
+#basically the path each transit line takes
+INSERT INTO `Transit_line_route`(`tlName`,`stopNum`,`origin`,`dest`) VALUES
+('Northeast Corridor', 1, 11, 12),
+('Northeast Corridor', 2, 12, 13),
+('Northeast Corridor', 3, 13, 14),
+('Northeast Corridor', 4, 14, 13),
+('Northeast Corridor', 5, 13, 12),
+('Northeast Corridor', 6, 12, 11);
+/*
+('Raritan Valley Line', 1, 11, 12),
+('Raritan Valley Line', 1, 11, 12),
+('Raritan Valley Line', 1, 11, 12),
+('Raritan Valley Line', 1, 11, 12),
+*/
+/*!40000 ALTER TABLE `Transit_line_route` ENABLE KEYS */;
 UNLOCK TABLES;
 
 LOCK TABLES `Schedule` WRITE;
@@ -282,15 +276,14 @@ UNLOCK TABLES;
 
 LOCK TABLES `transitLine` WRITE;
 /*!40000 ALTER TABLE `transitLine` DISABLE KEYS */;
-INSERT INTO `transitLine`(`transit-line-name`, `origin`, `destination`, `fixed fare`) VALUES
-('NorthEast Corridor' , 16, 25, 2.0),
-('Atlantic City Line' ,11, 15,5.0),
-('Raritan Valley Line' , 15, 18, 3.0),
-('Princeton Branch' , 18,  24, 10.0),
-('North Jersey Coastline',  29,  31,3.0),
-('Pascack Valley', 25,28,2.0);
+INSERT INTO `transitLine`(`tlName`, `origin`, `destination`, `fixed fare`) VALUES
+('Northeast Corridor', 11, 14, 2.0),
+('Raritan Valley Line', 15, 18, 3.0),
+('Keystone Service', 19, 22, 5.0),
+('North Jersey Coastline', 23, 25,3.0);
 /*!40000 ALTER TABLE `transitLine` ENABLE KEYS */;
 UNLOCK TABLES;
+
 
 LOCK TABLES `reservation` WRITE;
 /*!40000 ALTER TABLE `reservation` DISABLE KEYS */;
